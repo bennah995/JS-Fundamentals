39053b2dac89f98bd4aaf2112fc79d7c34232af2
Bennah <bennah955@gmail.com>
2025-05-25 23:38:57 +0300

    removed whilefor statement and used while exercise 6

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,20 +1,6 @@
-const message = ["C is fun", "Python is cool", "JavaScript is amazing"];
-
-while(i<message.length){
-  console.log(message[i]);
-  i++;
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let output = "";
+for (let i = 0; i < messages.length; i++) {
+  output += messages[i] + (i < messages.length - 1 ? "\n" : "");
 }
-
-
-
-
-// Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
-
-// The first line: “C is fun”
-// The second line: “”
-// The third line: “”
-// You must use console.log(...) to print all output
-// You are not allowed to use var
-// You are not allowed to use any if/else statement
-// You can use only one console.log
-// You must use a loop (while, for, etc.)
+console.log(output);
