1af724e159f184c32044a6e430d020edc4de6142
Bennah <bennah955@gmail.com>
2025-05-25 23:34:17 +0300

    exercise 6

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,19 @@
+const message = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+if(i=0; i<message.length; i++){
+  console.log(message[i]);
+}
+
+
+
+
+// Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+
+// The first line: “C is fun”
+// The second line: “”
+// The third line: “”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+// You are not allowed to use any if/else statement
+// You can use only one console.log
+// You must use a loop (while, for, etc.)
